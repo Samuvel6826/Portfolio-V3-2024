c6ce174c50fbbe75ed0a6e0a243884f872d06299
Samuvel Antony <samuvel6826@gmail.com>
2024-06-13 16:22:44 +0530

    Connected via Script

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { initialize } from 'react-ga';
 import App from './App.jsx'
 import './styles/index.css'
 
@@ -9,5 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-
-initialize('G-THT25FBCLL'); 
