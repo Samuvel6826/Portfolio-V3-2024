42be4d2f9b70b039482516912bba9d165356bf34
Samuvel Antony <samuvel6826@gmail.com>
2024-06-18 22:46:04 +0530

    Responsiveness added for Footer page

--- Frontend/src/components/pages/footer/Footer.jsx
+++ Frontend/src/components/pages/footer/Footer.jsx
@@ -28,126 +28,136 @@ const Footer = () => {
         </div>
     );
 
-    return <section id='footer' className='bg-tertiary mx-auto flex flex-col text-letter'>
-        <div id="container" className="container text-center mx-auto flex flex-col p-4 gap-5">
+    return <section id='footer' className='bg-tertiary mx-auto flex flex-col text-letter px-4'>
+        <div id="footer-container" className="container text-center mx-auto flex flex-col gap-5 py-4">
 
             <div id='foot-text-menu' className='max-md:hidden'>
                 <h3>MENU</h3>
 
                 <a href="#hero"><i className="uil uil-estate"></i>Home</a>
 
-
-                <a href="#about"><i className="uil uil-user"></i>About
-                </a>
-
+                <a href="#about"><i className="uil uil-user"></i>About</a>
 
                 <a href="#skills"><i className="uil uil-file-alt"></i>Skills</a>
 
-
                 <a href="#projects"><i className="uil uil-briefcase-alt"></i>Projects</a>
 
-
-
                 <a href="#portfolio"><i className="uil uil-scenery"></i>Portfolio</a>
-
 
                 <a href="#contact"><i className="uil uil-message"></i>Contact</a>
 
             </div>
 
-            <div id='foot-text-connect'>
+            <div id="media-container">
+                <div id='foot-text-connect'>
 
-                <h3>CONNECT</h3>
-                <div id="connect-container">
-                    <button>
+                    <h3>CONNECT</h3>
+
+                    <div id="connect-container">
                         <a href="https://wa.me/+919043251797?text=Hey%20Samuvel" target='blank'>
-                            <ImWhatsapp /> Whatsapp
+                            <button>
+                                <ImWhatsapp /> Whatsapp
+                            </button>
                         </a>
-                    </button>
 
-                    <button>
                         <a href="tel:+919043251797" target='blank'>
-                            <BiSolidPhoneCall /> +91 9043251797
+                            <button>
+                                <BiSolidPhoneCall /> +91 9043251797
+
+                            </button>
+                        </a>
+
+                        <a href="mailto:samuvel6826@gmail.com" target='blank'>
+                            <button>
+                                <SiGmail /> samuvel6826@gmail.com
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                <div id='foot-text-socials'>
+
+                    <h3>SOCIALS</h3>
+
+                    <button id='socials-container'>
+                        <a href="https://www.instagram.com/mr_fun_factory_24/" target='blank'>
+                            <FaInstagram />
+                        </a>
+
+                        <a href="https://www.facebook.com/samuvel68" target='blank'>
+                            <FaFacebook />
+                        </a>
+
+                        <a href="https://x.com/samuvel6826" target='blank'>
+                            <FaSquareXTwitter />
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/samuvelantony/" target='blank'>
+                            <FaLinkedin />
+                        </a>
+
+                        <a href="https://github.com/Samuvel6826" target='blank'>
+                            <FaGithub />
+                        </a>
+
+                        <a href="https://github.com/Samuvel6826" target='blank'>
+                            <FaDiscord />
                         </a>
                     </button>
 
-                    <button>
-                        <a href="mailto:samuvel6826@gmail.com" target='blank'>
-                            <SiGmail /> samuvel6826@gmail.com
-                        </a>
-                    </button>
                 </div>
             </div>
 
-            <div id='foot-text-socials'>
+            <div id="payments-container">
+                <div id="foot-btns">
 
-                <h3>SOCIALS</h3>
+                    <h3>PAYMENT</h3>
 
-                <button id='socials-container'>
-                    <a href="https://www.instagram.com/mr_fun_factory_24/" target='blank'>
-                        <FaInstagram />
+                    <a href="">
+                        <button>
+                            <LuIndianRupee />Indian Currency
+                        </button>
                     </a>
 
-                    <a href="https://www.facebook.com/samuvel68" target='blank'>
-                        <FaFacebook />
+                    <a href="">
+                        <button>
+                            <BiDollar />Foreign Currency
+                        </button>
                     </a>
 
-                    <a href="https://x.com/samuvel6826" target='blank'>
-                        <FaSquareXTwitter />
+                    <Space wrap>
+                        <Popover content={content} trigger="click">
+                            <button className='flex items-center justify-center text-center'><BiDollar />Crypto Currency</button>
+                        </Popover>
+                    </Space>
+
+                </div>
+
+                <div id="foot-btns">
+
+                    <h3>SUPPORT MY WORK</h3>
+
+                    <a href="https://ko-fi.com/samtocode24" target='_blank'>
+                        <button>
+                            <FaCoffee />  Buy me a ko-fi
+                        </button>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/samuvelantony/" target='blank'>
-                        <FaLinkedin />
+                    <a href="">
+                        <button>
+                            <FaGithub />  View Source code
+                        </button>
                     </a>
 
-                    <a href="https://github.com/Samuvel6826" target='blank'>
-                        <FaGithub />
-                    </a>
-
-                    <a href="https://github.com/Samuvel6826" target='blank'>
-                        <FaDiscord />
-                    </a>
-                </button>
-
-            </div>
-
-            <div id="foot-btns">
-
-                <h3>PAYMENT</h3>
-
-                <button>
-                    <a href=""><LuIndianRupee />Indian Currency</a>
-                </button>
-
-                <button>
-                    <a href=""><BiDollar />Foreign Currency</a>
-                </button>
-
-                <Space wrap>
-                    <Popover content={content} trigger="click">
-                        <button className='flex items-center justify-center text-center'><BiDollar />  Crypto Currency</button>
-                    </Popover>
-                </Space>
-
-            </div>
-
-            <div id="foot-btns">
-
-                <h3>SUPPORT MY WORK</h3>
-
-                <button>
-                    <a href="https://ko-fi.com/samtocode24"><FaCoffee />  Buy me a ko-fi</a>
-                </button>
-                <button>
-                    <a href=""><FaGithub />  View Source code</a>
-                </button>
+                </div>
             </div>
 
         </div>
 
-        <footer className="flex border-t-2 flex-col items-center justify-center">
-            <p className='flex items-center gap-2 mt-4'>Made with <FaReact /> <RiTailwindCssFill /> <FaNodeJs /> <SiExpress /> <SiMongodb /></p>
-            <p className=' px-4 mb-4 text-center'>
+        <footer id='footer-footer' className="flex border-t-2 flex-col items-center justify-center text-center p-2">
+            <p className='flex items-center gap-2'>Made with <FaReact /> <RiTailwindCssFill /> <FaNodeJs /> <SiExpress /> <SiMongodb /></p>
+            <p>
                 Made by Samuvel Antony. Copyrights &#169; 2024. All rights reserved.
             </p>
         </footer>
